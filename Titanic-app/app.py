865f658939c5bc935ada6b8d1e54d29ac0728b03
Yashvit Kumar <kumaryashvit@gmail.com>
2025-06-25 15:25:22 +0530

    v3 test

--- Titanic-app/app.py
+++ Titanic-app/app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import joblib
 
 # Load model
-model = joblib.load("titanic-model.joblib")
+model = joblib.load("./Titanic-app/titanic-model.joblib")
 
 # App title
 st.title("Titanic Survival Prediction")
